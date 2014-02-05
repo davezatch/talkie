bc5a017cc02a27f8069a81029b99c6040ea4ae63
David Carter <davezatch@gmail.com>
2014-02-05 22:12:37 +0100

    initial commit after yeoman generation

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global talkie, $*/
+
+
+window.talkie = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    talkie.init();
+});
