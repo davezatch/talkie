80369045316886c71437573169969afd5a60996f
David Carter <davezatch@gmail.com>
2014-02-09 17:45:12 +0100

    another crack at fonts

--- Gruntfile.js
+++ Gruntfile.js
@@ -276,6 +276,7 @@ module.exports = function (grunt) {
                         '<%= yeoman.dist %>/styles/{,*/}*.css',
                         '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}',
                         '/styles/fonts/{,*/}*.*',
+                        'bower_components/bootstrap/fonts/*.*'
                     ]
                 }
             }
